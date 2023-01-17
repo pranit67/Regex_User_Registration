ce292b016c0083dacdf22f704f0fdd4ff156bb94
pranit67 <pranit67@gmail.com>
2023-01-17 16:39:15 +0530

    Added: code for Welcome Message

---
+++ src/main/java/com/bridgelabz/user_registration/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.user_registration;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program..!");
+    }
+}
